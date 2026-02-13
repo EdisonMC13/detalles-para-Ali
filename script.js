4bf4dd5596d451ed5106f46c0721b3a6ef290c00
Edison Machay <edisonmachay9@gmail.com>
2026-02-12 19:48:24 -0500

    Arreglo de script

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const mensaje = document.getElementById('mensaje');
 
 boton.addEventListener('click', function() {
     titulo.innerText = "¡Te quiero mucho!";
-    mensaje.innerText = "Eres la persona más importante para mí. ❤️";
-    document.body.style.backgroundColor = "#b3ffc9"; ### Cambia el fondo a un color rosa suave
+    mensaje.innerText = "Eres la persona más importante para mí.";
+    document.body.style.backgroundColor = "#b3ffc9"; 
 });
